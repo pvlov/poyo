43ca82c4383856d949aa07af134cd041d2b15733
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2023-09-30 00:45:57 +0200

    Fix

--- src/main/java/org/pvlov/Main.java
+++ src/main/java/org/pvlov/Main.java
@@ -2,6 +2,6 @@ package org.pvlov;
 
 public class Main {
     public static void main(String[] args) {
-        new Bot("MTE1NzQzNTIyOTMyNjI4MjkyMw.GsLmyQ.vH_Rc_Sl-WEE_rKiB5ibQuiRPhQAnolCQgZSAg");
+        new Bot(System.getenv("DISCORD_TOKEN"));
     }
 }
