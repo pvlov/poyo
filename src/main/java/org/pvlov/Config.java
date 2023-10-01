21ca79baaf11ee987d61248e357f4a908dc6978d
Nico <nico.lintner@tum.de>
2023-10-01 16:37:58 +0200

    style fixes

--- src/main/java/org/pvlov/Config.java
+++ src/main/java/org/pvlov/Config.java
@@ -24,16 +24,16 @@ public class Config {
     public void loadConfig() {
         try {
             File file = new File(getWorkingDir() + "/config.yml");
-            if (!file.exists())
+            if (!file.exists()) {
                 file.createNewFile();
-
+            }
 			try (var inputStream = new FileInputStream(file)) {
 				data = new Yaml().loadAs(inputStream, Map.class);
 			} catch (RuntimeException e) {
 				LOG.error("Error parsing config.yml, make sure it's valid YAML");
 			}
 
-            // config file containing no tokens
+            // config file containing no / invalid tokens
             if (data == null) {
                 this.data = new HashMap<>();
                 return;
@@ -52,9 +52,9 @@ public class Config {
     public void saveConfig() {
         try {
             File file = new File(getWorkingDir() + "/config.yml");
-            if (!file.exists())
+            if (!file.exists()) {
                 file.createNewFile();
-
+            }
             var writer = new PrintWriter(file);
             new Yaml().dump(data, writer);
             writer.close();
@@ -64,36 +64,30 @@ public class Config {
     }
 
     public Optional<String> getString(String key) {
-        if (data.get(key) instanceof String val)
-            return Optional.of(val);
-
-        return Optional.empty();
+        return data.get(key) instanceof String val ? Optional.of(val) : Optional.empty();
     }
 
     // Maybe make this generic idk, would have to use a class parameter tho
     @SuppressWarnings("unchecked")
     public Optional<List<String>> getStringArray(String key) {
         if (data.containsKey(key) && data.get(key) instanceof List<?> list
-                && !list.isEmpty() && list.get(0) instanceof String)
+                && !list.isEmpty() && list.get(0) instanceof String) {
             return Optional.of((List<String>) list);
-
+        }
         return Optional.empty();
     }
 
     @SuppressWarnings("unchecked")
     public Optional<List<Long>> getLongArray(String key) {
         if (data.containsKey(key) && data.get(key) instanceof List<?> list
-                && !list.isEmpty() && list.get(0) instanceof Long)
+                && !list.isEmpty() && list.get(0) instanceof Long) {
             return Optional.of((List<Long>) list);
-
+        }
         return Optional.empty();
     }
 
     public Optional<Integer> getInt(String key) {
-        if (data.get(key) instanceof Integer val)
-            return Optional.of(val);
-
-        return Optional.empty();
+        return data.get(key) instanceof Integer val ? Optional.of(val) : Optional.empty();
     }
 
     public void setConfig(String key, String value) {
